8071e58ce08d3a9cd366d8b95f3d6e9a2b881de9
Matt-Weiss <weiss.matt@outlook.com>
2019-06-19 15:56:49 -0600

    Adds scope test functionality

--- app/declination_1.py
+++ app/declination_1.py
@@ -1,78 +1,42 @@
 import RPi.GPIO as GPIO
 import time
+
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-motor_1_A1_pin = 12 # black
-motor_1_A2_pin = 16 # yellow
-motor_1_B1_pin = 20 # green
-motor_1_B2_pin = 21 # white
-
-motor_2_A1_pin = 18 # black
-motor_2_A2_pin = 23 # yellow
-motor_2_B1_pin = 24 # green
-motor_2_B2_pin = 25 # white
 
 
-# adjust if different
-StepCount = 8
-Seq1 = list(range(0, StepCount))
-Seq1[0] = [0,1,0,0]
-Seq1[1] = [0,1,0,1]
-Seq1[2] = [0,0,0,1]
-Seq1[3] = [1,0,0,1]
-Seq1[4] = [1,0,0,0]
-Seq1[5] = [1,0,1,0]
-Seq1[6] = [0,0,1,0]
-Seq1[7] = [0,1,1,0]
+dir_pin = 21
+step_pin = 20
+enable_pin = 16
+microstep_pin = 26
 
-Seq2 = list(range(0, StepCount))
-Seq2[7] = [0,1,0,0]
-Seq2[6] = [0,1,0,1]
-Seq2[5] = [0,0,0,1]
-Seq2[4] = [1,0,0,1]
-Seq2[3] = [1,0,0,0]
-Seq2[2] = [1,0,1,0]
-Seq2[1] = [0,0,1,0]
-Seq2[0] = [0,1,1,0]
- 
- 
-GPIO.setup(motor_1_A1_pin, GPIO.OUT)
-GPIO.setup(motor_1_A2_pin, GPIO.OUT)
-GPIO.setup(motor_1_B1_pin, GPIO.OUT)
-GPIO.setup(motor_1_B2_pin, GPIO.OUT)
+GPIO.setup(dir_pin, GPIO.OUT)
+GPIO.setup(step_pin, GPIO.OUT)
+GPIO.setup(enable_pin, GPIO.OUT)
+GPIO.setup(microstep_pin, GPIO.OUT)
 
-GPIO.setup(motor_2_A1_pin, GPIO.OUT)
-GPIO.setup(motor_2_A2_pin, GPIO.OUT)
-GPIO.setup(motor_2_B1_pin, GPIO.OUT)
-GPIO.setup(motor_2_B2_pin, GPIO.OUT)
- 
- 
-def setStep(w1, w2, w3, w4):
-    GPIO.output(motor_1_A1_pin, w1)
-    GPIO.output(motor_1_A2_pin, w2)
-    GPIO.output(motor_1_B1_pin, w3)
-    GPIO.output(motor_1_B2_pin, w4)
-    
-def setStep2(w1, w2, w3, w4):
-    GPIO.output(motor_2_A1_pin, w1)
-    GPIO.output(motor_2_A2_pin, w2)
-    GPIO.output(motor_2_B1_pin, w3)
-    GPIO.output(motor_2_B2_pin, w4)
+GPIO.output(enable_pin, 1 ) 
+GPIO.output(dir_pin, 1 )
+GPIO.output(microstep_pin, 1 )
+
+GPIO.output(enable_pin, 0 )
  
 def forward(delay, steps):
     for i in list(range(steps)):
-        for j in list(range(StepCount)):
-            setStep(Seq1[j][0], Seq1[j][1], Seq1[j][2], Seq1[j][3])
-            setStep2(Seq2[j][0], Seq2[j][1], Seq2[j][2], Seq2[j][3])
-            time.sleep(delay/1000.0)
+        GPIO.output(dir_pin, 0 )
+        GPIO.output(step_pin, 1 )
+        time.sleep(delay/1000)
+        GPIO.output(step_pin, 0 )
+        time.sleep(delay/1000)     
  
 def backwards(delay, steps):
-    for i in list(range(steps)):
-        for j in reversed(list(range(StepCount))):
-            setStep(Seq1[j][0], Seq1[j][1], Seq1[j][2], Seq1[j][3])
-            setStep2(Seq2[j][0], Seq2[j][1], Seq2[j][2], Seq2[j][3])
-            time.sleep(delay/1000.0)
- 
+    for i in range(steps):
+        GPIO.output(dir_pin, 1 )
+        GPIO.output(step_pin, 1 )
+        time.sleep(delay/1000)
+        GPIO.output(step_pin, 0 )
+        time.sleep(delay/1000)
+     
 
  
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import app
 from app import declination_1
 from app import ascension
 from flask import request
+import time
 
 
 @app.route('/')
@@ -10,54 +11,24 @@ def root():
 
 @app.route('/scopetest')
 def scopetest():
- 
-    start_ra_steps = int(request.args.get('move_to_start_ra' ))
-    start_dec_steps = int(request.args.get('move_to_start_dec' ))
-    if start_dec_steps > 0 and start_ra_steps > 0:
-        declination_1.forward( 1, start_dec_steps )
-        ascension.forward( 1, start_ra_steps )
-    elif start_dec_steps < 0 and start_ra_steps > 0:
-        declination_1.backwards( 1, abs(start_dec_steps) )
-        ascension.forward( 1, start_ra_steps )
-    elif start_dec_steps > 0 and start_ra_steps < 0:
-        declination_1.forward( 1, start_dec_steps )
-        ascension.backwards( 1, abs(start_ra_steps) )
-    else:
-        declination_1.backwards( 1, abs(start_dec_steps)  )
-        ascension.backwards( 1, abs(start_ra_steps)  )
+    for i in list(range(1000)): 
+     declination_1.forward(2,1)
+     ascension.backwards(2,1)
+     
+    time.sleep(1)
     
-    track_dec_delay = int(request.args.get('track_dec_delay' ))
-    track_dec_steps = int(request.args.get('track_dec_steps' ))
-    track_ra_delay = int(request.args.get('track_ra_delay' ))
-    track_ra_steps = int(request.args.get('track_ra_steps' ))
-    if track_dec_steps > 0 and track_ra_steps > 0:
-        declination_1.forward( track_dec_delay, track_dec_steps )
-        ascension.forward( track_ra_delay, track_ra_steps )
-    elif track_dec_steps < 0 and track_ra_steps > 0:
-        declination_1.backwards( track_dec_delay, abs(track_dec_steps)  )
-        ascension.forward( track_ra_delay, track_ra_steps )
-    elif track_dec_steps > 0 and track_ra_steps < 0:
-        declination_1.forward( track_dec_delay, track_dec_steps )
-        ascension.backwards( track_ra_delay, abs(track_ra_steps)  )
-    else:
-        declination_1.backwards( track_dec_delay, abs(track_dec_steps)  )
-        ascension.backwards( track_ra_delay, abs(track_ra_steps)  )
-
-    return_home_ra_steps = int(request.args.get('return_home_ra_steps' ))
-    return_home_dec_steps = int(request.args.get('return_home_dec_steps' ))
-    if return_home_dec_steps > 0 and return_home_dec_steps > 0:
-        declination_1.forward( 1, return_home_dec_steps )
-        ascension.forward( 1, return_home_dec_steps )
-    elif return_home_dec_steps < 0 and return_home_dec_steps > 0:
-        declination_1.backwards( 1, abs(return_home_dec_steps)  )
-        ascension.forward( 1, return_home_dec_steps )
-    elif return_home_dec_steps > 0 and return_home_dec_steps < 0:
-        declination_1.forward( 1, return_home_dec_steps )
-        ascension.backwards( 1, abs(return_home_dec_steps)  )
-    else:
-        declination_1.backwards( 1, abs(return_home_dec_steps)  )
-        ascension.backwards( 1, abs(return_home_dec_steps)  )
+    for i in list(range(2000)):
+     declination_1.backwards(5,1)
+     ascension.forward(5,1)
+     
+    time.sleep(1)
+    
+    for i in list(range(1000)): 
+     declination_1.forward(2,1)
+     ascension.backwards(2,1)
+     
     return "success"
+    
 
 #@app.route('/scopenow')
 #def scopenow():
@@ -84,51 +55,51 @@ def scopetest():
 #@app.route('/scopetrack')
 #def scopetrack():
   
-    start_ra_steps = int(request.args.get('move_to_start_ra' ))
-    start_dec_steps = int(request.args.get('move_to_start_dec' ))
-    if start_dec_steps > 0 and start_ra_steps > 0:
-        declination_1.forward( 1, start_dec_steps )
-        ascension.forward( 1, start_ra_steps )
-    elif start_dec_steps < 0 and start_ra_steps > 0:
-        declination_1.backwards( 1, start_dec_steps )
-        ascension.forward( 1, start_ra_steps )
-    elif start_dec_steps > 0 and start_ra_steps < 0:
-        declination_1.forward( 1, start_dec_steps )
-        ascension.backwards( 1, start_ra_steps )
-    else:
-        declination_1.backwards( 1, start_dec_steps )
-        ascension.backwards( 1, start_ra_steps )
-
-    track_dec_delay = int(request.args.get('track_dec_delay' ))
-    track_dec_steps = int(request.args.get('track_dec_steps' ))
-    track_ra_delay = int(request.args.get('track_ra_delay' ))
-    track_ra_steps = int(request.args.get('track_ra_steps' ))
-    if track_dec_steps > 0 and track_ra_steps > 0:
-        declination_1.forward( track_dec_delay, track_dec_steps )
-        ascension.forward( track_ra_delay, track_ra_steps )
-    elif track_dec_steps < 0 and track_ra_steps > 0:
-        declination_1.backwards( track_dec_delay, track_dec_steps )
-        ascension.forward( track_ra_delay, track_ra_steps )
-    elif track_dec_steps > 0 and track_ra_steps < 0:
-        declination_1.forward( track_dec_delay, track_dec_steps )
-        ascension.backwards( track_ra_delay, track_ra_steps )
-    else:
-        declination_1.backwards( track_dec_delay, track_dec_steps )
-        ascension.backwards( track_ra_delay, track_ra_steps )
-  
-    return_home_ra_steps = int(request.args.get('return_home_ra_steps' ))
-    return_home_dec_steps = int(request.args.get('return_home_dec_steps' ))
-    if return_home_dec_steps > 0 and return_home_dec_steps > 0:
-        declination_1.forward( 1, return_home_dec_steps )
-        ascension.forward( 1, return_home_dec_steps )
-    elif return_home_dec_steps < 0 and return_home_dec_steps > 0:
-        declination_1.backwards( 1, return_home_dec_steps )
-        ascension.forward( 1, return_home_dec_steps )
-    elif return_home_dec_steps > 0 and return_home_dec_steps < 0:
-        declination_1.forward( 1, return_home_dec_steps )
-        ascension.backwards( 1, return_home_dec_steps )
-    else:
-        declination_1.backwards( 1, return_home_dec_steps )
-        ascension.backwards( 1, return_home_dec_steps )
-    return "success"
-    
+#    start_ra_steps = int(request.args.get('move_to_start_ra' ))
+#    start_dec_steps = int(request.args.get('move_to_start_dec' ))
+#    if start_dec_steps > 0 and start_ra_steps > 0:
+#        declination_1.forward( 1, start_dec_steps )
+#        ascension.forward( 1, start_ra_steps )
+#    elif start_dec_steps < 0 and start_ra_steps > 0:
+#        declination_1.backwards( 1, start_dec_steps )
+#        ascension.forward( 1, start_ra_steps )
+#    elif start_dec_steps > 0 and start_ra_steps < 0:
+#        declination_1.forward( 1, start_dec_steps )
+#        ascension.backwards( 1, start_ra_steps )
+#    else:
+#        declination_1.backwards( 1, start_dec_steps )
+#        ascension.backwards( 1, start_ra_steps )
+#
+#    track_dec_delay = int(request.args.get('track_dec_delay' ))
+#    track_dec_steps = int(request.args.get('track_dec_steps' ))
+#    track_ra_delay = int(request.args.get('track_ra_delay' ))
+#    track_ra_steps = int(request.args.get('track_ra_steps' ))
+#    if track_dec_steps > 0 and track_ra_steps > 0:
+#        declination_1.forward( track_dec_delay, track_dec_steps )
+#        ascension.forward( track_ra_delay, track_ra_steps )
+#    elif track_dec_steps < 0 and track_ra_steps > 0:
+#        declination_1.backwards( track_dec_delay, track_dec_steps )
+#        ascension.forward( track_ra_delay, track_ra_steps )
+#    elif track_dec_steps > 0 and track_ra_steps < 0:
+#        declination_1.forward( track_dec_delay, track_dec_steps )
+#        ascension.backwards( track_ra_delay, track_ra_steps )
+#    else:
+#        declination_1.backwards( track_dec_delay, track_dec_steps )
+#        ascension.backwards( track_ra_delay, track_ra_steps )
+#  
+#    return_home_ra_steps = int(request.args.get('return_home_ra_steps' ))
+#    return_home_dec_steps = int(request.args.get('return_home_dec_steps' ))
+#    if return_home_dec_steps > 0 and return_home_dec_steps > 0:
+#        declination_1.forward( 1, return_home_dec_steps )
+#        ascension.forward( 1, return_home_dec_steps )
+#    elif return_home_dec_steps < 0 and return_home_dec_steps > 0:
+#        declination_1.backwards( 1, return_home_dec_steps )
+#        ascension.forward( 1, return_home_dec_steps )
+#    elif return_home_dec_steps > 0 and return_home_dec_steps < 0:
+#        declination_1.forward( 1, return_home_dec_steps )
+#        ascension.backwards( 1, return_home_dec_steps )
+#    else:
+#        declination_1.backwards( 1, return_home_dec_steps )
+#        ascension.backwards( 1, return_home_dec_steps )
+#    return "success"
+#    
